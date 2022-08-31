a4783c5ce781d52e3acf5a2d2a65f13837a3c5de
unknown <marcosaureliofarias2023@gmail.com>
2022-08-31 17:44:36 -0300

    crud in memori

---
+++ src/app.js
@@ -0,0 +1,41 @@
+import express from 'express'
+
+const app = express();
+
+app.use(express.json())
+
+const livros = [
+    {id: 1, "titulo": "senhor dos Aneis"},
+    {id: 2, "titulo": "O Hobbit"}
+]
+
+app.get('/', (req, res) => {
+    res.status(200).send("Curso de node")
+})
+
+app.get('/livros', (req, res) => {
+    res.status(200).json(livros)
+})
+
+app.get('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.params.id)
+    res.json(livros[index])
+})
+
+app.post('/livros', (req, res) => {
+    livros.push(req.body)
+    res.status(201).send("Livro cadastrado com sucesso")
+})
+
+
+app.delete('/livros/:id', (req, res) => {
+    let {id} = req.params;
+    let index = buscaLivro(id)
+    livros.splice(index, 1);
+    res.send(`livro ${id} removido com sucesso`);
+})
+function buscaLivro(id){
+    return livros.findIndex(livro => livro.id == id)
+}
+
+export default app
